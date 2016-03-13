79b41c24da73b6eb8bc4468283792859a4861697
Trylobot <trylobot@gmail.com>
2016-03-13 14:44:12 -0500

    LICENSE, README updated; uploaded to clojars.org

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject hive "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject trylobot/hive "0.1.1-SNAPSHOT"
+  :description "Hive(tm) core logic library, rewritten in full native clojure"
+  :url "https://github.com/Trylobot/hive-clj"
+  :license {:name "Research License 1.0"
+            :url "https://github.com/Trylobot/hive-clj/blob/master/LICENSE.md"}
   :dependencies [
     [org.clojure/clojure "1.8.0"]
     [prismatic/schema "1.0.5"]]
