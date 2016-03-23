c6831f629b823e97ef90b7423821197e8abc4155
Trylobot <trylobot@gmail.com>
2016-03-23 07:59:43 -0500

    did stuff on the plane; check this over later

--- src/hive/core/domain/board.clj
+++ src/hive/core/domain/board.clj
@@ -280,9 +280,28 @@
           false
           (recur occupied new-frontier new-unexplored)) ))) )
 
-(defn is-range? "is the given value a valid hive range value? if so, return it in normalized form"
-  [r]
-    true )
+(def default-range "special range that represents no real restrictions"
+  {:min 0, :max :infinity})
+
+(defn is-range? "is the given value a simple range? if so, return the normalized form"
+  [r] (cond
+    (nil? r) ; nil --> {:min 0, :max :infinity}
+      default-range
+    (number? r) ; 3 --> {:min 3, :max 3}
+      {:min r, :max r}
+    (and (vector? r) (= 2 (count r)) (number? (nth r 0)) (number? (nth r 1))) ; [0, 3] --> {:min 0, :max 3}
+      {:min (nth r 0), :max (nth r 1)}
+    (and (map? r) (contains? :min) (contains? :max)) ; {:min 0, :max 3} --> identity
+      r ))
+
+(defn is-range-seq? "is the given value a sequence of simple ranges? if so, return the normalized forms of each; gaps filled by default"
+  [s, d] (cond
+    (and (or (vector? s) (seq? s)) (= d (count s)))
+      (mapv #(is-range? %) s)
+    (number? s)
+      (mapv #(is-range? s) (range d))
+    (is-range? s)
+      (mapv #(s) (range d)) ))
 
 (defn find-unique-paths-matching-conditions "find paths from start position matching length and height restrictions"
   [board start-position distance-range height-range]
